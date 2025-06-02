dc4f64c309fe0baccaea287ecd513c3a3805711d
KevinCPP <kevinncosby2@gmail.com>
2025-06-02 03:02:57 -0400

    .

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -12,7 +12,13 @@ export const NAVBAR_SIZE = 58;
 //  * in our WebView's CSS;
 //  * under `android/` (search for "BRAND_COLOR");
 //  * in `ios/**/Brand.colorset/Contents.json`.
-export const BRAND_COLOR = 'hsl(222, 99%, 69%)';
+
+// TandaPay brand color
+export const BRAND_COLOR = '#00B558';
+
+// Former zulip brand color:
+// export const BRAND_COLOR = 'hsl(222, 99%, 69%)';
+
 export const BORDER_COLOR = BRAND_COLOR;
 export const HIGHLIGHT_COLOR: string = Color(BRAND_COLOR).fade(0.5).toString();
 
@@ -21,3 +27,5 @@ export const QUARTER_COLOR = 'hsla(0, 0%, 50%, 0.25)';
 
 // Material warning color
 export const kWarningColor = 'hsl(40, 100%, 60%)';
+// Material error color
+export const kErrorColor = 'hsl(0, 100%, 60%)';
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -16,8 +16,10 @@ export const themeData: {| [name: ThemeName]: ThemeData |} = {
   dark: {
     themeName: 'dark',
     color: 'hsl(210, 11%, 85%)',
-    backgroundColor: 'hsl(212, 28%, 18%)',
-    cardColor: 'hsl(212, 31%, 21%)',
+    // former color: backgroundColor: 'hsl(125, 28%, 18%)',
+    backgroundColor: '#152D26',
+    // former color: cardColor: 'hsl(212, 31%, 21%)',
+    cardColor: '#01432A',
     // Dividers follow Material Design: opacity 12% black or 12% white.
     // See https://material.io/guidelines/components/dividers.html
     dividerColor: 'hsla(0, 0%, 100%, 0.12)',
